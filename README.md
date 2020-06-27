93c7350e05cec27caa6e42ab31b5f902ebbb0a63
lgm527 <laurellmccaffrey@gmail.com>
2020-06-27 11:45:25 -0400

    skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+# Git_Stats
+the baseball card for programmers
+
+## Goal
+utilize the GitHub API to display the statistics of a user
+
+### GitHub REST API v3
+https://developer.github.com/v3/
---
+++ main.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  function fetchUser() {
+    fetch("https://api.github.com/users/lgm527", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/vnd.github.v3+json",
+        "User-Agent": "lgm527",
+        "Authorization": "token " + config.gh_token,
+      }
+    })
+    .catch(error => console.error(error))
+    .then(res => res.json())
+    .then(renderCard)
+  }
+
+  fetchUser();
+
+  function renderCard({...user}) {
+    console.log(user);
+    document.getElementById("avatar").src = user.avatar_url;
+    document.getElementById("username").innerText += `User: ${user.login}`;
+    document.getElementById("created_at").innerText += `Coding since: ${new Date(user.created_at)}`;
+    document.getElementById("repos").innerText += `Repos: ${user.public_repos}`;
+    document.getElementById("gists").innerText += `Gists: ${user.public_gists}`;
+  }
+
+  // const card = document.getElementById("stat_card");
+
+});
